13e38514cbe05d8545f4062aafdb4e13fc4e776d
Afif Patel <afifpatel@hotmail.co.uk>
2022-09-23 20:49:08 +0100

    done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.static(path.join(__dirname, "./develop/public")));
